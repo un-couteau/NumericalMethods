d425846fd96009bba4d7181bbac7892126c451f9
Un Couteau <5453712-un_couteau@users.noreply.gitlab.com>
2023-05-18 02:31:27 +0500

    init

--- main.py
+++ main.py
@@ -1,12 +1,40 @@
-from src.simple_iteration import SimpleIteration
+import numpy as np
+import matplotlib.pyplot as plt
+from math import log
 
+def f(x):
+    return (log(x))**(9/7)
 
-def main() -> None:
-    matrix_a: list[list[float]] = [[-0.1, 0.2, -0.1], [0.1, -0.2, -0.2], [-0.2, 0.1, -0.1]]
-    matrix_b: list[float] = [1.6, -2.3, 1.5]
-    simple_iteration = SimpleIteration(matrix_a, matrix_b)
-    print(simple_iteration.get_result())
+x = [9, 12, 15]
+y = [f(x[0]), f(x[1]), f(x[2])]
 
+coefficients = np.polyfit(x, y, len(x) - 1)
 
-if __name__ == '__main__':
-    main()
+lagrange_poly = np.poly1d(coefficients)
+
+a = 11.5
+lagrange_value = lagrange_poly(a)
+
+x_values = np.linspace(x[0], x[2], 100)
+f_values = [f(x) for x in x_values]
+
+lagrange_values = lagrange_poly(x_values)
+
+plt.plot(x_values, lagrange_values, label='Полином Лагранжа')
+plt.plot(x_values, f_values, label='f(x)')
+plt.scatter(x, y, color='red', label='Узлы интерполяции')
+plt.scatter(a, lagrange_value, color='green', label='Значение полинома в точке a')
+plt.legend()
+plt.xlabel('x')
+plt.ylabel('y')
+plt.title('Интерполяция полиномом Лагранжа')
+plt.grid(True)
+plt.show()
+
+exact_value = f(a)
+
+interpolation_error = abs(exact_value - lagrange_value)
+
+print("Значение функции f(x) в точке a:", exact_value)
+print("Значение полинома Лагранжа в точке a:", lagrange_value)
+print("Погрешность интерполяции:", interpolation_error)
