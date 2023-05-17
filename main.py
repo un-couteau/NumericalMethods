f646a05ea03c7bc7000c65fde97330f3ce54684f
Un Couteau <5453712-un_couteau@users.noreply.gitlab.com>
2023-05-18 01:21:59 +0500

    need fix

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ from src.simple_iteration import SimpleIteration
 def main() -> None:
     matrix_a: list[list[float]] = [[-0.1, 0.2, -0.1], [0.1, -0.2, -0.2], [-0.2, 0.1, -0.1]]
     matrix_b: list[float] = [1.6, -2.3, 1.5]
-    simple_iteration = SimpleIteration(matrix_a, matrix_b, 0.01)
+    simple_iteration = SimpleIteration(matrix_a, matrix_b)
     print(simple_iteration.get_result())
 
 
