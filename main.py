67e17863548e2f52e8610fa5a8c44896259fe57d
Un Couteau <5453712-un_couteau@users.noreply.gitlab.com>
2023-05-18 06:27:15 +0500

    Init

--- main.py
+++ main.py
@@ -1,40 +1,16 @@
-import numpy as np
-import matplotlib.pyplot as plt
-from math import log
+from src.euler import Euler
 
-def f(x):
-    return (log(x))**(9/7)
 
-x = [9, 12, 15]
-y = [f(x[0]), f(x[1]), f(x[2])]
+def main() -> None:
+    a, b, c, d, l, m, n, p, q = 0, 8, 0, 6, 1, 9, 0, 3, 1
+    h = 0.2
+    euler = Euler(a, b, c, d, l, m, n, p, q, h)
+    # print(euler.get_result())
 
-coefficients = np.polyfit(x, y, len(x) - 1)
+    x_list, y_list = euler.get_result()
+    for x, y in zip(x_list, y_list):
+        print(f"x= {round(x, 1)}\ty= {round(y, 1)}")
 
-lagrange_poly = np.poly1d(coefficients)
 
-a = 11.5
-lagrange_value = lagrange_poly(a)
-
-x_values = np.linspace(x[0], x[2], 100)
-f_values = [f(x) for x in x_values]
-
-lagrange_values = lagrange_poly(x_values)
-
-plt.plot(x_values, lagrange_values, label='Полином Лагранжа')
-plt.plot(x_values, f_values, label='f(x)')
-plt.scatter(x, y, color='red', label='Узлы интерполяции')
-plt.scatter(a, lagrange_value, color='green', label='Значение полинома в точке a')
-plt.legend()
-plt.xlabel('x')
-plt.ylabel('y')
-plt.title('Интерполяция полиномом Лагранжа')
-plt.grid(True)
-plt.show()
-
-exact_value = f(a)
-
-interpolation_error = abs(exact_value - lagrange_value)
-
-print("Значение функции f(x) в точке a:", exact_value)
-print("Значение полинома Лагранжа в точке a:", lagrange_value)
-print("Погрешность интерполяции:", interpolation_error)
+if __name__ == '__main__':
+    main()
---
+++ src/euler.py
@@ -0,0 +1,37 @@
+import math
+
+
+class Euler:
+    def __init__(self, a: int, b: int, c: int, d: int, l: int, m: int, n: int, p: int, q: int, h: float) -> None:
+        self._a = a
+        self._b = b
+        self._c = c
+        self._d = d
+        self._l = l
+        self._m = m
+        self._n = n
+        self._p = p
+        self._q = q
+
+        self._h = h
+
+        self._y = 1
+        self._x = 0
+        self._y_list: list[float | int] = []
+        self._x_list: list[float | int] = []
+
+    def y(self) -> float:
+        return math.pow(-1, self._n + self._p + self._q) * float(f"{self._a}.{self._b}") * self._y + float(
+            f"{self._c}.{self._d}") * math.pow(self._x, 2) + float(f"{self._l}.{self._m}")
+
+    def get_result(self):
+        while self._x <= 2:
+            self._y_list.append(self._y)
+            self._x_list.append(self._x)
+
+            value: float = self.y()
+
+            self._y += self._h * value
+            self._x += self._h
+
+        return self._x_list, self._y_list
